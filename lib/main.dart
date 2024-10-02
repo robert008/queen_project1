01fb672a73162b9cff1744b9f83df0e6f3570b3a
robert <figo007007@gmail.com>
2024-10-02 22:23:50 +0800

    finish commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Queen project 8x8'),
     );
   }
 }
@@ -39,14 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +32,222 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<List<String>> solutions = [];
+  int total = 0;
+  String desc = '';
 
-  void _incrementCounter() {
+  void calculateRes() {
+   // solutions = solveQueens();
+    NQueensSolverIterative solver = NQueensSolverIterative(8);
+    // NQueensSolver solver = NQueensSolver(8);
+    List<List<int>> solutions = solver.solveNQueens();
+    print('解達總數量: ${solutions.length}\n');
+
+    total = solutions.length;
+
+    int n = total; // 想要呈現的數量
+    // 顯示前n個解答
+    for (int i = 0; i < (solutions.length < n ? solutions.length : n); i++) {
+
+      desc = desc + '// 解答 ${i + 1}\n' ;
+      String str = solver.getBoardStr(solutions[i]);
+      desc = desc + str +  '\n';
+    }
+
+    print(desc);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: <Widget>[
+              const Text(
+                '點擊右下角 Button 開始計算',
+              ),
+              SizedBox(height: 5,),
+              Text(
+                '總數量: $total',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              SizedBox(height: 5,),
+
+              Text(
+                '執行結果: $desc',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+          
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: calculateRes,
+        tooltip: 'run',
+        child: const Icon(Icons.run_circle_outlined,size: 50,),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class NQueensSolverIterative {
+  final int n;
+  List<List<int>> solutions = [];
+
+  NQueensSolverIterative(this.n);
+
+  List<List<int>> solveNQueens() {
+    // 初始化栈，每个元素是一个数组，表示当前的棋盘状态
+    // 每个状态包含两个列表：放置的列和当前行
+    List<int> board = List.filled(n, -1);
+    List<int> cols = List.filled(n, 0); // 记录每列是否被占用
+    List<int> diag1 = List.filled(2 * n - 1, 0); // 對角線
+    List<int> diag2 = List.filled(2 * n - 1, 0); // 對角線
+
+    int row = 0;
+    int col = 0;
+
+    while (row >= 0) {
+      while (col < n) {
+        if (cols[col] == 0 &&
+            diag1[row + col] == 0 &&
+            diag2[n - 1 + row - col] == 0) {
+          // 放置皇后
+          board[row] = col;
+          cols[col] = 1;
+          diag1[row + col] = 1;
+          diag2[n - 1 + row - col] = 1;
+
+          // 移動到下一行
+          row++;
+          col = 0;
+
+          // 如果所有 Q 都已放置，紀錄解决方案
+          if (row == n) {
+            solutions.add(List.from(board));
+            // 回溯到上一行
+            row--;
+            col = board[row] + 1;
+            cols[board[row]] = 0;
+            diag1[row + board[row]] = 0;
+            diag2[n - 1 + row - board[row]] = 0;
+          }
+
+          break;
+        } else {
+          col++;
+        }
+      }
+
+      if (col == n) {
+        // 無法在當前row找到合法的位置，回溯
+        row--;
+        if (row < 0) {
+          break;
+        }
+        col = board[row] + 1;
+        cols[board[row]] = 0;
+        diag1[row + board[row]] = 0;
+        diag2[n - 1 + row - board[row]] = 0;
+      }
+    }
+
+    return solutions;
+  }
+
+  /// 產生棋盤布局字串
+  String getBoardStr(List<int> board) {
+    String str = '';
+    for (int row = 0; row < n; row++) {
+      String line = '';
+      for (int col = 0; col < n; col++) {
+        if (board[row] == col) {
+          line += ' Q ';
+        } else {
+          line += ' . ';
+        }
+      }
+      // print(line);
+      str = str + line + '\n';
+    }
+    return str;
+  }
+}
+
+class NQueensSolver {
+  int n;
+  List<List<int>> solutions = [];
+
+  NQueensSolver(this.n);
+
+  List<List<int>> solveNQueens() {
+    List<int> board = List.filled(n, -1);
+    _backtrack(0, board, <List<int>>[]);
+    return solutions;
+  }
+
+  /// 回溯算法的核心函数
+  void _backtrack(int row, List<int> board, List<List<int>> solutions) {
+    if (row == n) {
+      // 找到一個解决方案，複製並加入到结果中
+      solutions.add(List.from(board));
+      return;
+    }
+
+    for (int col = 0; col < n; col++) {
+      if (_isSafe(row, col, board)) {
+        board[row] = col;
+        _backtrack(row + 1, board, solutions);
+        board[row] = -1; // 回溯
+      }
+    }
+
+
+    if (row == 0) {
+      this.solutions = solutions;
+    }
+  }
+
+  /// 检查在 (row, col) 放置 Q 是否安全
+  bool _isSafe(int row, int col, List<int> board) {
+    for (int i = 0; i < row; i++) {
+      if (board[i] == col ||
+          board[i] - i == col - row ||
+          board[i] + i == col + row) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /// 產生棋盤布局字串
+  String getBoardStr(List<int> board) {
+    String str = '';
+
+    for (int row = 0; row < n; row++) {
+      String line = '';
+      for (int col = 0; col < n; col++) {
+        if (board[row] == col) {
+          line += ' Q ';
+        } else {
+          line += ' . ';
+        }
+      }
+      // print(line);
+      str = str + line + '\n';
+    }
+    return str;
   }
 }
